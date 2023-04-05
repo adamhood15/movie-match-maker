e249707216fca23af66eccc90883a6f02e5b0c56
ctalv <122413805+ctalv@users.noreply.github.com>
2023-04-04 20:57:49 -0500

    added base random movie selector

--- script.js
+++ script.js
@@ -17,35 +17,40 @@ const ratingEl = $('#rating');
 
 
 //go button
-$(document).ready(function() {
-	distanceEl.on("input", function() {
+$(document).ready(function () {
+	distanceEl.on("input", function () {
 		var sliderValue = $(this).val();
 		$('#distanceDisplay').text("Within " + sliderValue + " miles");
-		});
-	submitBtnEl.on("click", function() {
+	});
+	submitBtnEl.on("click", function () {
 		localStorage.setItem('name', nameInputEl.val());
 		localStorage.setItem('address', addressInputEl.val());
 		localStorage.setItem('distance', distanceEl.val());
 		window.location.href = "cuisine.html";
 	});
-	});
+});
 
 
+function randomMovie(movies) {
+	console.log(movies)
+	Math.round(Math.random(length))
+}
 
+movieAPICall()
 
+function movieAPICall() {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '470bbc37cbmsh159b75e4fb9ceb9p1dd3fbjsn904a4308001a',
+			'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+		}
+	};
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '470bbc37cbmsh159b75e4fb9ceb9p1dd3fbjsn904a4308001a',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	}
-};
-
-fetch('https://moviesdatabase.p.rapidapi.com/titles/utils/genres', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	fetch('https://moviesdatabase.p.rapidapi.com/titles/utils/genres', options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
 
 	const optionsb = {
 		method: 'GET',
@@ -54,37 +59,38 @@ fetch('https://moviesdatabase.p.rapidapi.com/titles/utils/genres', options)
 			'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
 		}
 	};
-	
+
 	fetch('https://moviesdatabase.p.rapidapi.com/titles/utils/lists', optionsb)
 		.then(response => response.json())
 		.then(response => console.log(response))
 		.catch(err => console.error(err));
 
-		const optionsc = {
-			method: 'GET',
-			headers: {
-				'X-RapidAPI-Key': '470bbc37cbmsh159b75e4fb9ceb9p1dd3fbjsn904a4308001a',
-				'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-			}
-		};
-		
-		fetch('https://moviesdatabase.p.rapidapi.com/titles/utils/titleTypes', optionsc)
-			.then(response => response.json())
-			.then(response => console.log(response))
-			.catch(err => console.error(err));
+	const optionsc = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '470bbc37cbmsh159b75e4fb9ceb9p1dd3fbjsn904a4308001a',
+			'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+		}
+	};
 
-			const optionsd = {
-				method: 'GET',
-				headers: {
-					'X-RapidAPI-Key': '470bbc37cbmsh159b75e4fb9ceb9p1dd3fbjsn904a4308001a',
-					'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-				}
-			};
-			
-			fetch('https://moviesdatabase.p.rapidapi.com/titles?titleType=movie&list=top_rated_250&sort=year.decr&startYear=2000', optionsd)
-				.then(response => response.json())
-				.then(response => console.log(response))
-				.catch(err => console.error(err));
+	fetch('https://moviesdatabase.p.rapidapi.com/titles/utils/titleTypes', optionsc)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
+
+	const optionsd = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '470bbc37cbmsh159b75e4fb9ceb9p1dd3fbjsn904a4308001a',
+			'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+		}
+	};
+
+	fetch('https://moviesdatabase.p.rapidapi.com/titles?titleType=movie&list=top_rated_250&sort=year.decr&startYear=2000', optionsd)
+		.then(response => response.json())
+		.then(response => randomMovie(response))
+		.catch(err => console.error(err));
+}
 //Unsplash API
 
 const accessKey = 'zMuuOQCazY49v22R_yBjWGu_68ZE3qmVG7V011pysrg';
@@ -96,16 +102,16 @@ const apiUrl = `https://api.unsplash.com/search/photos?page=1&query=${query}&cli
 // Fetch data from the API endpoint and display the first image
 // ID 'foodIMG' set for image div endpoint, tested and works, but currently not in HTML
 fetch(apiUrl)
-  	.then(response => response.json())
-  	.then(data => {
-    	const image = data.results[0];
-    	const img = document.createElement('img');
-    	img.src = image.urls.regular;
-    	img.alt = image.alt_description;
-    	const foodIMG = document.querySelector('#foodIMG');
-    	foodIMG.appendChild(img);
-  	})
-  	.catch(error => console.log(error));
+	.then(response => response.json())
+	.then(data => {
+		const image = data.results[0];
+		const img = document.createElement('img');
+		img.src = image.urls.regular;
+		img.alt = image.alt_description;
+		const foodIMG = document.querySelector('#foodIMG');
+		foodIMG.appendChild(img);
+	})
+	.catch(error => console.log(error));
 
 //local storage for the userInput
 var userName = 'Adam';
